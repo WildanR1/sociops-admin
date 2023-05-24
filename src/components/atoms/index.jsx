bb00ee2703ca0f863b5a5d9f79df762ebf4a00c9
yahoogm <yaho.garfield@gmail.com>
2023-05-25 00:45:47 +0700

    yahoo: make modular export components

---
+++ src/components/atoms/index.jsx
@@ -0,0 +1,3 @@
+export { default as Button } from "./Button/Button/Button.atom";
+export { default as InputText } from "./Input/InputText/InputText.atom";
+export { default as InputIcon } from "./Input/InputIcon/InputIcon.atom";
