a47582743335cef251eb2d508da27f84b7369239
Wildan Rachmadoni <wildanrachmadoni@gmail.com>
2023-05-27 14:46:42 +0700

    feat: slicing design dashboard volunteer

--- src/components/atoms/index.jsx
+++ src/components/atoms/index.jsx
@@ -2,3 +2,4 @@ export { default as Button } from "./Button/Button/Button.atom";
 export { default as InputText } from "./Input/InputText/InputText.atom";
 export { default as InputIcon } from "./Input/InputIcon/InputIcon.atom";
 export { default as ButtonBigIcon } from "./Button/ButtonBigIcon/ButtonBigIcon.atom";
+export { default as ProgramItem } from "./PrgramItem/ProgramItem.atom";
