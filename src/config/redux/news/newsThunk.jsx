716e2706d0b86eea726e68ddf94d1507fa5edc10
Moh Alvian Noer <mohammadalviannoer@gmail.com>
2023-06-19 10:12:46 +0700

    fix feat create news post data

--- src/config/redux/news/newsThunk.jsx
+++ src/config/redux/news/newsThunk.jsx
@@ -19,3 +19,19 @@ export const retrieveDetailNews = createAsyncThunk(
     }
   },
 );
+
+export const createNews = createAsyncThunk("news/createNews", async (param) => {
+  try {
+    const req = await axios.post(`${process.env.API_URL}/news`, param.data, {
+      headers: {
+        Authorization: `Bearer ${param.token}`,
+      },
+    });
+
+    const res = await req.data;
+    const data = await res.data;
+    return data;
+  } catch (error) {
+    throw error;
+  }
+});
