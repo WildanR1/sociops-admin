2891c660bb3c519229a9ca67dc81e822177b5759
yahoogm <yaho.garfield@gmail.com>
2023-06-22 16:13:31 +0700

    feat: riwayat program volunteer

--- src/config/redux/volunteer/volunteerSelector.jsx
+++ src/config/redux/volunteer/volunteerSelector.jsx
@@ -2,3 +2,9 @@ import { useSelector } from "react-redux";
 
 export const useDetailVolunteer = () =>
   useSelector((state) => state.volunteer.volunteerDetail);
+
+export const useRiwayatProgramVolunteer = () =>
+  useSelector((state) => state.volunteer.riwayatProgramVolunteer);
+
+export const useRiwayatProgramVolunteerType = () =>
+  useSelector((state) => state.volunteer.type);
