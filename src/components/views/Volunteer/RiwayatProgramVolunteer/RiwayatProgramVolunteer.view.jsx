cb1a22f0025f65f134548c0304cd51308fe893f4
yahoogm <yaho.garfield@gmail.com>
2023-06-22 17:36:36 +0700

    feat: riwayat program fundraising & volunteer and library sharp & encoding

--- src/components/views/Volunteer/RiwayatProgramVolunteer/RiwayatProgramVolunteer.view.jsx
+++ src/components/views/Volunteer/RiwayatProgramVolunteer/RiwayatProgramVolunteer.view.jsx
@@ -1,8 +1,38 @@
+"use client";
+
 import { DefaultTemplate } from "@/components/template";
 import { TableV1 } from "@/components/organisms";
 import { Button, ButtonBack, TableV1Row } from "@/components/atoms";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { retrieveRiwayatProgramVolunteer } from "@/config/redux/volunteer/volunteerThunk";
+import { useUserToken } from "@/config/redux/user/userSelector";
+import { useState } from "react";
+import ReactPaginate from "react-paginate";
+import {
+  useRiwayatProgramVolunteer,
+  useRiwayatProgramVolunteerType,
+} from "@/config/redux/volunteer/volunteerSelector";
+import moment from "moment";
 
-const RiwayatProgramVolunteer = () => {
+const RiwayatProgram = () => {
+  const dispatch = useDispatch();
+  const token = useUserToken();
+  const riwayatVolunter = useRiwayatProgramVolunteer();
+  const type = useRiwayatProgramVolunteerType();
+
+  const loading = type === retrieveRiwayatProgramVolunteer.pending.type;
+
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handlePaginate = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+  const pageCount = 10;
+
+  useEffect(() => {
+    dispatch(retrieveRiwayatProgramVolunteer({ currentPage, token }));
+  }, [dispatch, token, currentPage]);
   return (
     <DefaultTemplate>
       <div className='mb-[7px] pb-4 flex items-center'>
@@ -20,106 +50,72 @@ const RiwayatProgramVolunteer = () => {
           header4='Tanggal'
           header5='Status'
         >
-          <TableV1Row
-            variant='primary'
-            no='1'
-            nama='#JustDoIt, Lakukan Olahraga dengan Nyaman Bersama Kami'
-            deskripsi='Proin et euismod diam. Duis fermentum felis nisi, ut lobortis lectus mollis non. Integer pellentesque erat eu diam pharetra auctor id et nulla. Nam sodales arcu nec blandit fringilla. Ut vitae ligula vel lectus ultrices tempus ut id sem. Etiam egestas lacus scelerisque augue congue, sed rutrum sem lobortis. Pellentesque vel enim ante. Quisque hendrerit lobortis neque, ac tempor dui elementum vel. Duis '
-            tanggal='6 Mei 2023'
-            button={
-              <Button
-                text='Ditolak'
-                variant='error-600'
-                width='w-full'
-                fontSize='medium'
-                size='medium'
-              />
-            }
-          />
-          <TableV1Row
-            variant='primary'
-            no='2'
-            nama='#JustDoIt, Lakukan Olahraga dengan Nyaman Bersama Kami'
-            deskripsi='Proin et euismod diam. Duis fermentum felis nisi, ut lobortis lectus mollis non. Integer pellentesque erat eu diam pharetra auctor id et nulla. Nam sodales arcu nec blandit fringilla. Ut vitae ligula vel lectus ultrices tempus ut id sem. Etiam egestas lacus scelerisque augue congue, sed rutrum sem lobortis. Pellentesque vel enim ante. Quisque hendrerit lobortis neque, ac tempor dui elementum vel. Duis '
-            tanggal='6 Mei 2023'
-            button={
-              <Button
-                text='Diterima'
-                variant='success-600'
-                width='w-full'
-                fontSize='medium'
-                size='medium'
-              />
-            }
-          />
-          <TableV1Row
-            variant='primary'
-            no='3'
-            nama='#JustDoIt, Lakukan Olahraga dengan Nyaman Bersama Kami'
-            deskripsi='Proin et euismod diam. Duis fermentum felis nisi, ut lobortis lectus mollis non. Integer pellentesque erat eu diam pharetra auctor id et nulla. Nam sodales arcu nec blandit fringilla. Ut vitae ligula vel lectus ultrices tempus ut id sem. Etiam egestas lacus scelerisque augue congue, sed rutrum sem lobortis. Pellentesque vel enim ante. Quisque hendrerit lobortis neque, ac tempor dui elementum vel. Duis '
-            tanggal='6 Mei 2023'
-            button={
-              <Button
-                text='Diterima'
-                variant='success-600'
-                width='w-full'
-                fontSize='medium'
-                size='medium'
-              />
-            }
-          />
-          <TableV1Row
-            variant='primary'
-            no='4'
-            nama='#JustDoIt, Lakukan Olahraga dengan Nyaman Bersama Kami'
-            deskripsi='Proin et euismod diam. Duis fermentum felis nisi, ut lobortis lectus mollis non. Integer pellentesque erat eu diam pharetra auctor id et nulla. Nam sodales arcu nec blandit fringilla. Ut vitae ligula vel lectus ultrices tempus ut id sem. Etiam egestas lacus scelerisque augue congue, sed rutrum sem lobortis. Pellentesque vel enim ante. Quisque hendrerit lobortis neque, ac tempor dui elementum vel. Duis '
-            tanggal='6 Mei 2023'
-            button={
-              <Button
-                text='Ditolak'
-                variant='error-600'
-                width='w-full'
-                fontSize='medium'
-                size='medium'
-              />
-            }
-          />
-          <TableV1Row
-            variant='primary'
-            no='5'
-            nama='#JustDoIt, Lakukan Olahraga dengan Nyaman Bersama Kami'
-            deskripsi='Proin et euismod diam. Duis fermentum felis nisi, ut lobortis lectus mollis non. Integer pellentesque erat eu diam pharetra auctor id et nulla. Nam sodales arcu nec blandit fringilla. Ut vitae ligula vel lectus ultrices tempus ut id sem. Etiam egestas lacus scelerisque augue congue, sed rutrum sem lobortis. Pellentesque vel enim ante. Quisque hendrerit lobortis neque, ac tempor dui elementum vel. Duis '
-            tanggal='6 Mei 2023'
-            button={
-              <Button
-                text='Ditolak'
-                variant='error-600'
-                width='w-full'
-                fontSize='medium'
-                size='medium'
-              />
-            }
-          />
-          <TableV1Row
-            variant='primary'
-            no='6'
-            nama='#JustDoIt, Lakukan Olahraga dengan Nyaman Bersama Kami'
-            deskripsi='Proin et euismod diam. Duis fermentum felis nisi, ut lobortis lectus mollis non. Integer pellentesque erat eu diam pharetra auctor id et nulla. Nam sodales arcu nec blandit fringilla. Ut vitae ligula vel lectus ultrices tempus ut id sem. Etiam egestas lacus scelerisque augue congue, sed rutrum sem lobortis. Pellentesque vel enim ante. Quisque hendrerit lobortis neque, ac tempor dui elementum vel. Duis '
-            tanggal='6 Mei 2023'
-            button={
-              <Button
-                text='Diterima'
-                variant='success-600'
-                width='w-full'
-                fontSize='medium'
-                size='medium'
-              />
-            }
-          />
+          <>
+            {loading ? (
+              <td colSpan={5} className='py-10 text-center'>
+                <div
+                  className='radial-progress animate-spin'
+                  style={{ "--value": 70 }}
+                ></div>
+              </td>
+            ) : riwayatVolunter.length !== 0 ? (
+              riwayatVolunter.map((riwayat, idx) => {
+                return (
+                  <TableV1Row
+                    key={idx}
+                    variant='primary'
+                    no={(currentPage + 1) * 5 - 5 + (idx + 1)}
+                    nama={riwayat.title}
+                    deskripsi={riwayat.description}
+                    tanggal={moment(riwayat.created_at).format("D MMM YYYY")}
+                    button={
+                      <Button
+                        text={riwayat.status}
+                        variant={
+                          riwayat.status === "REJECTED"
+                            ? "error-600"
+                            : "success-600"
+                        }
+                        width='w-full'
+                        fontSize='medium'
+                        size='medium'
+                      />
+                    }
+                  />
+                );
+              })
+            ) : (
+              <td colSpan={5} className='capitalize text-center py-10 text-lg'>
+                data tidak ada
+              </td>
+            )}
+          </>
         </TableV1>
+
+        <div className='flex justify-center mt-4'>
+          <ReactPaginate
+            pageCount={pageCount} // Jumlah total halaman
+            pageRangeDisplayed={3} // Jumlah halaman yang ditampilkan
+            marginPagesDisplayed={2} // Jumlah halaman di sekitar halaman aktif yang ditampilkan
+            onPageChange={handlePaginate} // Fungsi yang dipanggil saat halaman berubah
+            containerClassName='join'
+            activeClassName='btn-active'
+            nextLabel='>>'
+            previousLabel='<<'
+            pageClassName='join-item btn btn-square btn-outline'
+            pageLinkClassName='w-full h-full flex justify-center items-center'
+            previousClassName='join-item btn btn-square btn-outline'
+            previousLinkClassName='w-full h-full flex justify-center items-center'
+            nextClassName='join-item btn btn-square btn-outline'
+            nextLinkClassName='w-full h-full flex justify-center items-center'
+            breakLabel='...'
+            breakClassName='join-item btn btn-square btn-outline'
+            breakLinkClassName='w-full h-full flex justify-center items-center'
+          />
+        </div>
       </div>
     </DefaultTemplate>
   );
 };
 
-export default RiwayatProgramVolunteer;
+export default RiwayatProgram;
