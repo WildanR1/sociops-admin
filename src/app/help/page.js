ff5d9b1e1305cf177effd885a5c15feb5544602f
EBay12Y <ekabayus12@gmail.com>
2023-06-26 01:26:00 +0700

    add help page's

---
+++ src/app/help/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const help = () => {
+  return (
+    <div className='flex justify-center items-center w-full h-screen'>
+      <iframe
+        src='https://embed.lottiefiles.com/animation/120952'
+        className='w-full h-full'
+      ></iframe>
+    </div>
+  );
+};
+
+export default help;
