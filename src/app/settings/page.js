fe345b0ee51d1798a53c4abff4588a171ca94941
EBay12Y <ekabayus12@gmail.com>
2023-06-26 01:55:07 +0700

    add button back for help, settings, profile  page's

--- src/app/settings/page.js
+++ src/app/settings/page.js
@@ -1,11 +1,19 @@
+import { ButtonBack } from "@/components/atoms";
+
 const page = () => {
   return (
-    <div className='flex justify-center items-center w-full h-screen'>
-      <iframe
-        src='https://embed.lottiefiles.com/animation/120952'
-        className='w-full h-full'
-      ></iframe>
-    </div>
+    <>
+      <div className='my-5 mx-8 absolute z-10 font-bold items-center flex'>
+        <ButtonBack /> &nbsp; Kembali
+      </div>
+      <div className='flex flex-col items-center justify-center w-full h-screen'>
+        <iframe
+          src='https://embed.lottiefiles.com/animation/120952'
+          className='w-full h-full'
+          title='Lottie Animation'
+        ></iframe>
+      </div>
+    </>
   );
 };
 
