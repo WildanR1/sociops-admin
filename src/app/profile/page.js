4197a3b54c1223096866af6e360bbe63c53d5ba3
EBay12Y <ekabayus12@gmail.com>
2023-06-26 01:28:35 +0700

    add profile page's

---
+++ src/app/profile/page.js
@@ -0,0 +1,12 @@
+const page = () => {
+  return (
+    <div className='flex justify-center items-center w-full h-screen'>
+      <iframe
+        src='https://embed.lottiefiles.com/animation/120952'
+        className='w-full h-full'
+      ></iframe>
+    </div>
+  );
+};
+
+export default page;
